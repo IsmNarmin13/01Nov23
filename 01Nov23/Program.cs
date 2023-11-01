3d529142d47e2791b6f6445a01ecfbbdd6e08e1e
ismnarmin13 <narminvi@code.edu.az>
2023-11-01 13:43:21 +0400

    first commit

---
+++ 01Nov23/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] ResizeArray(int[] arr, int a)
+    {
+        int[] newArr = new int[a];
+        int elementsToCopy = Math.Min(arr.Length, a);
+        for (int i = 0; i < elementsToCopy; i++)
+        {
+            newArr[i] = arr[i];
+        }
+        return newArr;
+    }
+int[] myArray = { 1, 2, 3, 4, 5 };
+myArray = ResizeArray(myArray, 3);
+Console.WriteLine(string.Join(", ", myArray));
